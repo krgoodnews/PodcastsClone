4df305f437df7e3a7e48c7aac73d72251560c4ef
guinnesswift <krgoodnews@gmail.com>
2018-03-15 20:52:53 +0900

    Player Detail View.swift&xib

--- CopyPodcasts/Controllers/EpisodesController.swift
+++ CopyPodcasts/Controllers/EpisodesController.swift
@@ -52,6 +52,18 @@ class EpisodesController: UITableViewController {
 	
 	// MARK: - UITableView
 	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let episode = self.episodes[indexPath.row]
+		print("Trying to play episode:", episode.title)
+		
+		let window = UIApplication.shared.keyWindow
+		
+		let playerDetailsView = Bundle.main.loadNibNamed("PlayerDetailView", owner: self, options: nil)?.first as! PlayerDetailView
+		playerDetailsView.episode = episode
+		playerDetailsView.frame = self.view.frame
+		window?.addSubview(playerDetailsView)
+	}
+	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return self.episodes.count
 	}
---
+++ CopyPodcasts/Views/PlayerDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  PlayerDetailView.swift
+//  CopyPodcasts
+//
+//  Created by Goodnews on 2018. 3. 15..
+//  Copyright © 2018년 krgoodnews. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailView: UIView {
+	
+	var episode: Episode! {
+		didSet {
+			titleLabel.text = episode.title
+			
+			let url = URL(string: episode.imageUrl?.toSecureHTTPS() ?? "")
+			episodeImageView.sd_setImage(with: url)
+		}
+	}
+	
+	@IBAction func handleDismiss(_ sender: Any) {
+		self.removeFromSuperview()
+	}
+	
+	@IBOutlet weak var episodeImageView: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+}
