cc08cb665dc837acc101bd8fc09a89b63a0b4600
guinnesswift <krgoodnews@gmail.com>
2018-03-28 13:27:11 +0900

    autoPlay, play&pause

--- CopyPodcasts/Models/Episode.swift
+++ CopyPodcasts/Models/Episode.swift
@@ -14,11 +14,14 @@ struct Episode {
 	let pubDate: Date
 	let description: String
 	let author: String
+	let streamUrl: String
 	
 	var imageUrl: String?
 	
 	
 	init(feedItem: RSSFeedItem) {
+		self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
+		
 		self.title = feedItem.title ?? ""
 		self.pubDate = feedItem.pubDate ?? Date()
 		self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
--- CopyPodcasts/Views/PlayerDetailView.swift
+++ CopyPodcasts/Views/PlayerDetailView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailView: UIView {
 	
@@ -14,10 +15,29 @@ class PlayerDetailView: UIView {
 		didSet {
 			titleLabel.text = episode.title
 			authorLabel.text = episode.author
+			
+			playEpisode()
+			
 			let url = URL(string: episode.imageUrl?.toSecureHTTPS() ?? "")
 			episodeImageView.sd_setImage(with: url)
 		}
 	}
+	
+	fileprivate func playEpisode() {
+		
+		
+		print("Trying to play episode at url:", episode.streamUrl)
+		guard let url = URL(string: episode.streamUrl) else { return }
+		let playerItem = AVPlayerItem(url: url)
+		player.replaceCurrentItem(with: playerItem)
+		player.play()
+	}
+	
+	let player: AVPlayer = {
+		let avPlayer = AVPlayer()
+		avPlayer.automaticallyWaitsToMinimizeStalling = false // 다운로드가 전부 완료되지 않아도 재생 가능하면 바로 재생 가능
+		return avPlayer
+	}()
 	
 	@IBAction func handleDismiss(_ sender: Any) {
 		self.removeFromSuperview()
@@ -32,5 +52,21 @@ class PlayerDetailView: UIView {
 		}
 	}
 	@IBOutlet weak var authorLabel: UILabel!
-	@IBOutlet weak var playPauseButton: UIButton!
+	@IBOutlet weak var playPauseButton: UIButton! {
+		didSet {
+			playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+			playPauseButton.addTarget(self, action: #selector(didTapPlayPause), for: .touchUpInside)
+		}
+	}
+	
+	@objc func didTapPlayPause() {
+		print("Trying to play and pause")
+		if player.timeControlStatus == .paused {
+			player.play()
+			playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+		} else {
+			player.pause()
+			playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+		}
+	}
 }
