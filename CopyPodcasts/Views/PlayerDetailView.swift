932b2d07423b887661e302d0e7b5ce73beb62195
guinnesswift <krgoodnews@gmail.com>
2018-04-17 14:43:57 +0900

    Maximize & Minimize using Drag and Drop

--- CopyPodcasts/Views/PlayerDetailView.swift
+++ CopyPodcasts/Views/PlayerDetailView.swift
@@ -63,11 +63,14 @@ class PlayerDetailView: UIView {
 		self.currentTimeSlider.value = Float(percentage)
 	}
 	
+	var panGesture: UIPanGestureRecognizer!
+	
 	override func awakeFromNib() {
 		super.awakeFromNib()
 		
 		addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapMaximize)))
-		addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+		panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+		addGestureRecognizer(panGesture)
 		
 		observePlayerCurrentTime()
 		
@@ -84,31 +87,51 @@ class PlayerDetailView: UIView {
 	
 	@objc private func handlePan(gesture: UIPanGestureRecognizer) {
 		
-		if gesture.state == .began {
-			print("Began")
-		} else if gesture.state == .changed {
+		if gesture.state == .changed {
+			handlePanChanged(gesture: gesture)
 			
-			let translation = gesture.translation(in: self.superview)
-			self.transform = CGAffineTransform(translationX: 0, y: translation.y)
-			
-			self.miniPlayerView.alpha = 1 + translation.y / 200
-			self.maximizedStackView.alpha = -translation.y / 200
-			
-			print(translation.y)
 		} else if gesture.state == .ended {
-			print("Ended")
-			UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-				self.transform = .identity
+			
+			handlePanEnded(gesture: gesture)
+		}
+	}
+	
+	func handlePanChanged(gesture: UIPanGestureRecognizer) {
+		let translation = gesture.translation(in: self.superview)
+		self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+		
+		self.miniPlayerView.alpha = 1 + translation.y / 200
+		self.maximizedStackView.alpha = -translation.y / 200
+		
+	}
+	
+	func handlePanEnded(gesture: UIPanGestureRecognizer) {
+		let translation = gesture.translation(in: self.superview)
+		let velocity = gesture.velocity(in: self.superview)
+		
+		
+		print("Ended:", velocity.y)
+		
+		UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+			self.transform = .identity
+			
+			if translation.y < -200 || velocity.y < -500 {
+				let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
+				
+				mainTabBarController?.maximizePlayerDetails(nil)
+				gesture.isEnabled = false
+			} else {
 				self.miniPlayerView.alpha = 1
 				self.maximizedStackView.alpha = 0
-			})
-		}
+			}
+			
+		})
 	}
 	
 	@objc private func didTapMaximize() {
 		let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
 		mainTabBarController?.maximizePlayerDetails(nil)
-		print("Tapping to maximize")
+		panGesture.isEnabled = false
 	}
 	
 	static func initFromNib() -> PlayerDetailView {
@@ -146,6 +169,8 @@ class PlayerDetailView: UIView {
 //		self.removeFromSuperview()
 		let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
 		mainTabBarController?.minimizePlayerDetails()
+		
+		panGesture.isEnabled = true
 	}
 	
 	
